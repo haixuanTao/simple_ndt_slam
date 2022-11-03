81e083018af1dfa359765aae1dd2984f5677ebf1
Xavier Tao <tao.xavier@outlook.com>
2022-11-03 16:32:00 -0700

    Update setup_lib.sh

--- assets/scripts/setup_lib.sh
+++ assets/scripts/setup_lib.sh
@@ -3,6 +3,16 @@
 apt install -y ros-$ROS_DISTRO-tf2-geometry-msgs
 apt install -y libomp-dev
 
+# install glog
+mkdir -p ~/workspace/lib
+cd /~workspace/lib
+git clone https://github.com/google/glog.git
+cd glog
+git fetch --all --tags
+git checkout tags/v0.4.0 -b v0.4.0
+mkdir build && cd build
+cmake .. && make
+make install
 
 git clone https://github.com/gflags/gflags.git
 cd gflags
@@ -10,4 +20,4 @@ mkdir build && cd build
 cmake .. -DBUILD_SHARED_LIBS=ON && make
 make install
 cd ~
-rm -rf /workspace/lib
+rm -rf ~/workspace/lib
